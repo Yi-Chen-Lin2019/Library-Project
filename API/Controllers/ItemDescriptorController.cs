ac8c3c0c78e0111a3225bb2578736822c457669a
Yi-Chen-Lin2019 <58112802+Yi-Chen-Lin2019@users.noreply.github.com>
2022-05-20 15:32:10 +0200

    LPFTD-11 add getItemDescriptor and getItemDescriptors

--- API/Controllers/ItemDescriptorController.cs
+++ API/Controllers/ItemDescriptorController.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Application;
+using Application.Features.ItemDescriptor.Queries.GetAllItemDescriptors;
+using Application.Features.ItemDescriptor.Queries.GetItemDescriptor;
 using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
@@ -13,6 +17,22 @@ namespace API.Controllers
         {
             this.dispatcher = dispatcher;
             this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetItemDescriptors()
+        {
+            GetAllItemDescriptorsQuery query = new GetAllItemDescriptorsQuery();
+            var result = await this.dispatcher.Dispatch(query);
+            return FromResult(result);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetItemDescriptor(int id)
+        {
+            var result = await this.dispatcher.Dispatch(new GetItemDescriptorQuery(id));
+            return FromResult(result);
         }
     }
 }
