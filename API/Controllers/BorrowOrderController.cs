0c65a1e4de7e8015b4304f8955214eb2b819e07f
arsendr <minezspeletajs@gmail.com>
2022-05-20 12:53:04 +0200

    added API project to solution

---
+++ API/Controllers/BorrowOrderController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BorrowOrderController : ControllerBase
+    {
+        // GET: api/<BorrowOrderController>
+        [HttpGet]
+        [Route("BorrowOrder")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<BorrowOrderController>/5
+        //[HttpGet("{OrderId}")]
+        [HttpGet]
+        [Route("BorrowOrder/{OrderId}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<BorrowOrderController>
+        [HttpPost]
+        [Route("BorrowOrder")]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BorrowOrderController>/5
+        //[HttpPut("{OrderId}")]
+        [HttpPut]
+        [Route("BorrowOrder/{OrderId}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BorrowOrderController>/5
+        //[HttpDelete("{OrderId}")]
+        [HttpDelete]
+        [Route("BorrowOrder/{OrderId}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ API/Controllers/LibUserController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibUserController : ControllerBase
+    {
+        // GET: api/<LibUserController>
+        [HttpGet]
+        [Route("LibUser")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<LibUserController>/5
+        [HttpGet]
+        [Route("LibUser/{SSN}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LibUserController>
+        [HttpPost]
+        [Route("LibUser")]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LibUserController>/5
+        [HttpPut]
+        [Route("LibUser/{SSN}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LibUserController>/5
+        [HttpDelete]
+        [Route("LibUser/{SSN}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
