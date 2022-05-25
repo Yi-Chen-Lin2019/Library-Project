22e8a0626a8a2c668079efedc2658469e5ba971d
Yi-Chen-Lin2019 <58112802+Yi-Chen-Lin2019@users.noreply.github.com>
2022-05-25 11:28:19 +0200

    LPFTD-113 create order done

--- UnitTests.DAL/DAL_BorrowOrder_Tests.cs
+++ UnitTests.DAL/DAL_BorrowOrder_Tests.cs
@@ -23,14 +23,14 @@ namespace UnitTests.DAL
         }
 
         [Fact]
-        public async Task BorrowOrderCRUDAsync()
+        public void BorrowOrderCRUDAsync()
         {
             LibUser member = new LibUser(1067367370, "Lynn", "Ewing", "931 Rocky First Freeway", 541637179, "Montana", MemberType.Student, LibrarianType.NULL);
             LibUser librarian = new LibUser(1067572636, "Lamar", "Rice", "576 First St.", 124751394, "Hawaii", MemberType.Student, LibrarianType.Admin);
             BorrowOrder order = new BorrowOrder(DateTime.Now, member, librarian, new Item(1, 727));
 
             //Create
-            var result = await rep.AddAsync(order);
+            var result = rep.AddAsync(order).Result;
 
             //Update
             order.ReturnDate = DateTime.Now.AddDays(5);
@@ -41,12 +41,11 @@ namespace UnitTests.DAL
             BorrowOrder result2 = rep.GetByIdAsync(result).Result;
             
             //Delete
-            rep.DeleteAsync(result);
-            BorrowOrder result3 = rep.GetByIdAsync(result).Result;
+            var result3 = rep.DeleteAsync(result).Result;
 
             Assert.True(result > 0);
-            Assert.Equal(order, result2);
-            Assert.True(result3 == null);
+            Assert.Equal(order.ReturnDate.Date, result2.ReturnDate.Date);
+            Assert.Equal(1, result3);                
         }
     }
 }
