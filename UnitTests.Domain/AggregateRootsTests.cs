b34b67c5dbb80873037f98187d0d00549fef63e9
Yi-Chen-Lin2019 <58112802+Yi-Chen-Lin2019@users.noreply.github.com>
2022-06-01 14:18:27 +0200

    update assert in domain test

--- UnitTests.Domain/AggregateRootsTests.cs
+++ UnitTests.Domain/AggregateRootsTests.cs
@@ -11,14 +11,19 @@ namespace UnitTests.Domain
         public void BorrowOrder()
         {
             BorrowOrder borrowOrder = new BorrowOrder(1, DateTime.Now);
-            Assert.True(borrowOrder != null);
+            Assert.Equal(1, borrowOrder.OrderID);
         }
 
         [Fact]
         public void ItemDescriptor()
         {
-            ItemDescriptor itemD = new Book(1999, "Stephen Strange", "Magic Forces", BorrowType.Stationary, "Article describing dark forces of the universe.", "Demon", 4323442, "Magic", 1);
-            Assert.True(itemD != null);
+            ItemDescriptor itemD = new Book(1999, "Stephen Strange", "Magic Forces",
+                BorrowType.Stationary, "Article describing dark forces of the universe.", "Demon", 4323442, "Magic", 1);
+            Assert.Equal("Stephen Strange", itemD.Author);
+            Assert.Equal("Magic Forces", itemD.Title);
+            Assert.Equal(BorrowType.Stationary, itemD.Borrow_Type);
+            Assert.Equal("Article describing dark forces of the universe.", itemD.Description);
+            Assert.Equal("Demon", itemD.Publisher);
         }
 
         [Fact]
@@ -26,7 +31,14 @@ namespace UnitTests.Domain
         {
             LibUser user = new LibUser(991224452, "Peter", "Parker", "Queens 10432", 757773884, "Empire State Universtiy",
             MemberType.Student, LibrarianType.NULL);
-            Assert.True(user != null);
+            Assert.Equal(991224452, user.SSN);
+            Assert.Equal("Peter", user.FName);
+            Assert.Equal("Parker", user.Surname);
+            Assert.Equal("Queens 10432", user.Address);
+            Assert.Equal(757773884, user.Phone);
+            Assert.Equal("Empire State Universtiy", user.Campus);
+            Assert.Equal(MemberType.Student, user.MemberType);
+            Assert.Equal(LibrarianType.NULL, user.LibrarianType);
         }
 
         [Fact]
@@ -34,7 +46,14 @@ namespace UnitTests.Domain
         {
             LibUser user = new LibUser(991224452, "Peter", "Parker", "Queens 10432", 757773884, "Empire State Universtiy",
             MemberType.NULL, LibrarianType.HeadLibrarian);
-            Assert.True(user != null);
+            Assert.Equal(991224452, user.SSN);
+            Assert.Equal("Peter", user.FName);
+            Assert.Equal("Parker", user.Surname);
+            Assert.Equal("Queens 10432", user.Address);
+            Assert.Equal(757773884, user.Phone);
+            Assert.Equal("Empire State Universtiy", user.Campus);
+            Assert.Equal(MemberType.NULL, user.MemberType);
+            Assert.Equal(LibrarianType.HeadLibrarian, user.LibrarianType);
         }
 
         [Fact]
@@ -42,7 +61,14 @@ namespace UnitTests.Domain
         {
             LibUser user = new LibUser(991224452, "Peter", "Parker", "Queens 10432", 757773884, "Empire State Universtiy",
             MemberType.Professor, LibrarianType.Admin);
-            Assert.True(user != null);
+            Assert.Equal(991224452, user.SSN);
+            Assert.Equal("Peter", user.FName);
+            Assert.Equal("Parker", user.Surname);
+            Assert.Equal("Queens 10432", user.Address);
+            Assert.Equal(757773884, user.Phone);
+            Assert.Equal("Empire State Universtiy", user.Campus);
+            Assert.Equal(MemberType.Professor, user.MemberType);
+            Assert.Equal(LibrarianType.Admin, user.LibrarianType);
         }
 
         [Fact]
@@ -51,8 +77,8 @@ namespace UnitTests.Domain
             LibUser user = new LibUser(991224452, "Peter", "Parker", "Queens 10432", 757773884, "Empire State Universtiy",
             MemberType.Professor, LibrarianType.Admin);
             Reservation reservation = new Reservation(15, DateTime.Now, new Item(1), user);
-
-            Assert.True(reservation != null);
+            Assert.Equal(MemberType.Professor, reservation.Borrower.MemberType);
+            Assert.Equal(15, reservation.ReservationID);
         }
     }
 }
--- UnitTests.Domain/EntitiesTests.cs
+++ UnitTests.Domain/EntitiesTests.cs
@@ -18,29 +18,44 @@ namespace UnitTests.Domain
         [Fact]
         public void Book()
         {
-            Book book = new Book(1999, "Stephen Strange", "Magic Forces", BorrowType.Stationary, "Article describing dark forces of the universe.", "Demon", 4323442, "Magic", 1);
-            Assert.True(book != null);
+            Book book = new Book(1999, "Stephen Strange", "Magic Forces",
+                BorrowType.Stationary, "Article describing dark forces of the universe.", "Demon", 4323442, "Magic", 1);
+            Assert.Equal(1999, book.Year);
+            Assert.Equal("Stephen Strange", book.Author);
+            Assert.Equal("Magic Forces", book.Title);
+            Assert.Equal(BorrowType.Stationary, book.Borrow_Type);
+            Assert.Equal("Article describing dark forces of the universe.", book.Description);
+            Assert.Equal("Demon", book.Publisher);
+            Assert.Equal(4323442, book.ISBN);
+            Assert.Equal("Magic", book.Subject);
+            Assert.Equal(1, book.Edition);
         }
 
         [Fact]
         public void Map()
         {
             Map map = new Map(-1040, "Stephen Strange", "Map of the Multiverse", BorrowType.Borrow, "The map of the multiverse!", "Sorcerer Supreme");
-            Assert.True(map != null);
+            Assert.Equal(-1040, map.Year);
+            Assert.Equal("Stephen Strange", map.Author);
+            Assert.Equal("Map of the Multiverse", map.Title);
+            Assert.Equal(BorrowType.Borrow, map.Borrow_Type);
+            Assert.Equal("The map of the multiverse!", map.Description);
+            Assert.Equal("Sorcerer Supreme", map.Publisher);
         }
 
         [Fact]
         public void Item()
         {
             Item item = new Item(34);
-            Assert.True(item != null);
+            Assert.Equal(34, item.ItemID);
         }
 
         [Fact]
         public void LibraryCard()
         {
             LibraryCard libraryCard = new LibraryCard(1, DateTime.Now, 999900125);
-            Assert.True(libraryCard != null);
+            Assert.Equal(999900125, libraryCard.UserSSN);
+            Assert.Equal(1, libraryCard.CardID);
         }
     }
 }
